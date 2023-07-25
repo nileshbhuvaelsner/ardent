038aa5f492ee3438581173477a4c8e9f9b054b79
nileshbhuvaelsner <nileshbhuva@elsner.in>
2023-07-25 16:54:36 +0530

    px to rem changes style

--- functions.php
+++ functions.php
@@ -333,7 +333,7 @@ function ardent_scripts()
 	}
 
 	wp_enqueue_script('ardent-theme', get_template_directory_uri() . '/assets/js/theme-all' . $min_ext . '.js', $deps, $version, true);
-	wp_enqueue_script('ardent-theme', get_template_directory_uri() . '/script.js', $deps, $version, true);
+	wp_enqueue_script('ardent-theme-script', get_template_directory_uri() . '/script.js', $deps, $version, true);
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
